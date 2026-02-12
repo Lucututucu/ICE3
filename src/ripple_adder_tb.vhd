f12af8d6e1ddc8fb0eb5e634df260d0937cc95c8
Lucututucu <C27Lucas.Briggs@afacademy.af.edu>
2026-02-11 19:35:58 -0700

    Updated work on ICE 3.

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,6 +49,16 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
+       w_addends <= x"6E"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"4" and w_Cout = '1') report "bad with 6E+0" severity failure;
+       w_addends <= x"32"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"6" and w_Cout = '0') report "bad with 32+1" severity failure;
+       w_addends <= x"A9"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"3" and w_Cout = '1') report "bad with A9+0" severity failure;
+       w_addends <= x"C1"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"E" and w_Cout = '0') report "bad with C1+1" severity failure;
+       w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with Carry in" severity failure;
 	
 		wait; -- wait forever
 	end process;	
